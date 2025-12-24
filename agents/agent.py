5846c198e5b3aa9224525fa25d76e305af341791
cristianmgm7 <cristianmgm7@gmail.com>
2025-12-23 19:32:55 -0500

    Add requirements file and update agent configuration
    - Introduced requirements.txt to manage project dependencies.
    - Updated .env file to include GITHUB_TOKEN for GitHub API access.
    - Modified agent.py to create a GitHub agent with integrated MCP toolset for enhanced functionality.

--- agents/agent.py
+++ agents/agent.py
@@ -1,8 +1,24 @@
-from google.adk.agents.llm_agent import Agent
+import os
+from google.adk.agents import Agent
+from google.adk.tools.mcp_tool import McpToolset
+from google.adk.tools.mcp_tool.mcp_session_manager import StreamableHTTPServerParams
+
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 
 root_agent = Agent(
-    model='gemini-2.5-flash',
-    name='root_agent',
-    description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+    model="gemini-2.5-flash",
+    name="github_agent",
+    instruction="Help users get information from GitHub",
+    tools=[
+        McpToolset(
+            connection_params=StreamableHTTPServerParams(
+                url="https://api.githubcopilot.com/mcp/",
+                headers={
+                    "Authorization": f"Bearer {GITHUB_TOKEN}",
+                    "X-MCP-Toolsets": "all",
+                    "X-MCP-Readonly": "true"
+                },
+            ),
+        )
+    ],
 )
